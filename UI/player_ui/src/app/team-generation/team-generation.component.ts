81a8811cb218c91a934962e5baa132983174d945
HussainPythonista <jaheerkalanthar816@gmail.com>
2024-05-25 12:03:20 +0530

    button for team generation

--- UI/player_ui/src/app/team-generation/team-generation.component.ts
+++ UI/player_ui/src/app/team-generation/team-generation.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-team-generation',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './team-generation.component.html',
   styleUrl: './team-generation.component.css'
 })
 export class TeamGenerationComponent {
 
+  number_of_teams:number=0
+  generate_teams(){
+    console.log(`${this.number_of_teams}  will generate`)
+  }
 }
